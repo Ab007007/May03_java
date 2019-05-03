be1909c633f9eedf64f68dcbca59a91dfb855df1
Aravinda <aru03.info@gmail.com>
2019-05-03 08:31:11 +0530

    java 03_05)2019

---
+++ src/com/synehcron/jt/conditionalstmts/IFDemo.java
@@ -0,0 +1,31 @@
+package com.synehcron.jt.conditionalstmts;
+
+import java.util.Scanner;
+
+public class IFDemo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Finding largest number");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first number");
+		int a =  sc.nextInt();
+
+		System.out.println("Enter the second number");
+		int b =  sc.nextInt();
+		
+		if(a>b)
+		{
+			System.out.println("First Number a  is greater number");
+		}
+		else if(b>a)
+		{
+			System.out.println("Second Number a  is greater number");
+		}
+		else
+		{
+			System.out.println(" Both are equal ");
+		}
+	}
+}
